9d8169e2e776c22835c2888e75c7a5999223e320
larsdekooter <102305474+larsdekooter@users.noreply.github.com>
2023-12-24 10:41:38 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -130,7 +130,7 @@ def train():
                 record = score
                 agent.model.save()
 
-            print('Game', agent.n_games, 'Won', score, "Epsilon", agent.epsilon, "%", round(aiMoves/totalMoves * 100.0, 5))
+            print('Game', agent.n_games, 'Won', score, "Epsilon", agent.epsilon, "%", round(aiMoves/totalMoves * 100.0, 5), aiMoves, totalMoves)
             plot_scores.append(score)
             total_score += score
             mean_score = total_score / agent.n_games
