a5fb626ee5b3ddd04392b0c06427401e41ece3a7
Lars de Kooter <lars@larsdekooter.nl>
2023-12-29 17:44:04 +0100

    3layer model

--- model.py
+++ model.py
@@ -9,13 +9,13 @@ class Linear_QNet(nn.Module):
         super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, hidden_size)
-        self.linear3 = nn.Linear(hidden_size, hidden_size)
+        # self.linear3 = nn.Linear(hidden_size, hidden_size)
         self.linear4 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         x = F.relu(self.linear1(x))
         x = F.relu(self.linear2(x))
-        x = F.relu(self.linear3(x))
+        # x = F.relu(self.linear3(x))
         x = self.linear4(x)
         return x
 
